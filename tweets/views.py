f0779e5e47f3427d9980a6e42525a0faccf3a38c
lalebdi <loai.alebdi@gmail.com>
2021-02-13 14:33:54 -0500

    URL Routing

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello World</h1>")
