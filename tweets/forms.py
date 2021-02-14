560107843219f80c8f74b776d9558867892ea47a
lalebdi <loai.alebdi@gmail.com>
2021-02-14 14:08:18 -0500

    Tweet Create Form

---
+++ tweets/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.forms import fields
+from .models import Tweet
+
+MAX_TWEET_LENGTH = 240
+
+class TweetForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        fields = ['content']
+
+        def clean_content(self):
+            """ This going to validate the content to a strict 240 char length """
+            content = self.cleaned_data.get("content")
+            if len(content) > MAX_TWEET_LENGTH:
+                raise forms.ValidationError("This tweet is too long! Max limit 240 charaters")
+            return content
