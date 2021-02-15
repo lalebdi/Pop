d257281aa51c0f574fdee5f3499f7d4a3587cb99
lalebdi <loai.alebdi@gmail.com>
2021-02-15 14:41:32 -0500

    serilaze the model objects

--- tweets/models.py
+++ tweets/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+import random
 
 # Create your models here.
 
 class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0, 2021)
+        }
