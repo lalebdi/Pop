7c86e812ed085d24596243c122a7a55f05a5d67a
lalebdi <loai.alebdi@gmail.com>
2021-02-15 14:49:33 -0500

    class fir reverse order

--- tweets/models.py
+++ tweets/models.py
@@ -7,6 +7,9 @@ class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
+    class Meta:
+        ordering = ['-id'] # reverses the order
+
     def serialize(self):
         return {
             "id": self.id,
