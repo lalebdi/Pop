669b23461512b331b8f2b8a1d0dd4e547e15239a
lalebdi <loai.alebdi@gmail.com>
2021-02-15 20:52:37 -0500

    fix the typo in the error message

--- tweets/forms.py
+++ tweets/forms.py
@@ -13,5 +13,5 @@ class TweetForm(forms.ModelForm):
         """ This going to validate the content to a strict 240 char length """
         content = self.cleaned_data.get("content")
         if len(content) > MAX_TWEET_LENGTH:
-            raise forms.ValidationError("This tweet is too long! Max limit 240 charaters")
+            raise forms.ValidationError("This tweet is too long! Max limit 240 charaters.")
         return content
