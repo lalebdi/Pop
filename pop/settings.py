f9062ecec31b56a68a51d4fdfabc19d1899893fe
lalebdi <loai.alebdi@gmail.com>
2021-02-16 10:56:29 -0500

    Django Framework

--- pop/settings.py
+++ pop/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # THird party
+    'rest_framwork',
+    # Internal apps
     'tweets',
 ]
 
