7771c7d6fc61dd95a2c3f3970e3f550b490edf98
lalebdi <loai.alebdi@gmail.com>
2021-02-16 13:45:38 -0500

    new likes model and timestamp for the posts

--- tweets/models.py
+++ tweets/models.py
@@ -6,10 +6,19 @@ import random
 
 User = settings.AUTH_USER_MODEL
 
+
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) # this is so many users can have many tweets. CASCADE will remove all the user's tweets. If I want to keep a history change the params to null=Truem and on_delete=models.SET_NULL
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike) # Many to Many meanns one tweet can have many users
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self): # this will show the contents in the admin page instead of the id
         return self.content
