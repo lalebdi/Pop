6bb32ee612a87a6e301fe179ef151f28ad635708
lalebdi <loai.alebdi@gmail.com>
2021-02-16 12:54:50 -0500

    delete posts

--- tweets/views.py
+++ tweets/views.py
@@ -42,6 +42,21 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
     return Response(serializer.data, status=200)
 
 
+
+@api_view(['DELETE', 'POST'])
+@permission_classes([IsAuthenticated]) # if authenticated, they have access to this
+def tweet_delete_view(request, tweet_id, *args, **kwargs):
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+    qs = qs.filter(user=request.user)
+    if not qs.exists(): # permission will not be denied
+        return Response({"message": "You cannot delete this tweet"}, status=401)
+    obj = qs.first()
+    obj.delete()
+    return Response({"message": "Tweet removed"}, status=200)
+
+
 @api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
