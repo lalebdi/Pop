d0c753a1fa027b7b179cdc61de409465138860ea
lalebdi <loai.alebdi@gmail.com>
2021-02-15 20:24:38 -0500

    fix the bug

--- tweets/forms.py
+++ tweets/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import fields
+
 from .models import Tweet
 
 MAX_TWEET_LENGTH = 240
@@ -8,10 +8,10 @@ class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ['content']
-
-        def clean_content(self):
-            """ This going to validate the content to a strict 240 char length """
-            content = self.cleaned_data.get("content")
-            if len(content) > MAX_TWEET_LENGTH:
-                raise forms.ValidationError("This tweet is too long! Max limit 240 charaters")
-            return content
+    
+    def clean_content(self):
+        """ This going to validate the content to a strict 240 char length """
+        content = self.cleaned_data.get("content")
+        if len(content) > MAX_TWEET_LENGTH:
+            raise forms.ValidationError("This tweet is too long! Max limit 240 charaters")
+        return content
