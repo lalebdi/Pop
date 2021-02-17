6da1efe5f62efdfb3aef7313559b75366e24d02f
lalebdi <loai.alebdi@gmail.com>
2021-02-17 10:44:17 -0500

    convert to RESST API

---
+++ tweets/urls.py
@@ -0,0 +1,27 @@
+"""pop URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from .views import home_view, tweet_action_view, tweet_delete_view, tweet_list_view, tweet_detail_view, tweet_create_view
+
+urlpatterns = [
+    path('', tweet_list_view),
+    path('action', tweet_action_view),
+    path('create', tweet_create_view),
+    path('<int:tweet_id>', tweet_detail_view),
+    path('<int:tweet_id>/delete', tweet_delete_view),
+]
