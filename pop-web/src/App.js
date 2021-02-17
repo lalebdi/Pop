937b318e35714249154ad2fcc26d1bd4423daf1d
lalebdi <loai.alebdi@gmail.com>
2021-02-17 15:22:03 -0500

    got the xhr request but with CORS error

--- pop-web/src/App.js
+++ pop-web/src/App.js
@@ -1,13 +1,51 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function loadTweets(callback){
+  const xhr = new XMLHttpRequest()
+  const method = 'GET'
+  const url = 'http://127.0.0.1:8000/api/tweets/'
+  const responseType = "json"
+
+  xhr.responseType = responseType
+  xhr.open(method, url)
+  xhr.onload = function(){
+  callback(xhr.response, xhr.status)
+  }
+  xhr.onerror = function(e) {
+    console.log(e)
+    callback({"message": "The request encoutered an error"}, 400)
+  }
+  xhr.send()
+
+  }
+
+
 function App() {
+  const [ tweets, setTweets ] = useState([])
+
+  useEffect(() => {
+    const myCallback = (response, status) =>{
+      console.log(response, status)
+      if (status === 200){
+        setTweets(response)
+      }
+    }
+    loadTweets(myCallback)
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {tweets.map((tweet, index)=>{
+            return <li>{tweet.content}</li>
+          })}
         </p>
         <a
           className="App-link"
