5c870acb402510e59caa69321963ba25772b4232
lalebdi <loai.alebdi@gmail.com>
2021-02-17 10:44:13 -0500

    convert to RESST API

--- pop/urls.py
+++ pop/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from tweets.views import home_view, tweet_action_view, tweet_delete_view, tweet_list_view, tweet_detail_view, tweet_create_view
 
@@ -24,6 +24,7 @@ urlpatterns = [
     path('create-tweet', tweet_create_view),
     path('tweets/', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
-    path('api/tweets/action', tweet_action_view),
-    path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
+    # path('api/tweets/action', tweet_action_view),
+    # path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
+    path('api/tweets/', include('tweets.urls'))
 ]
