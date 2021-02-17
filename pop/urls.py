38d5064e3fc369346acab17494c9db6f156e4179
lalebdi <loai.alebdi@gmail.com>
2021-02-16 19:46:07 -0500

    handling actions in JavaScript

--- pop/urls.py
+++ pop/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from tweets.views import home_view, tweet_delete_view, tweet_list_view, tweet_detail_view, tweet_create_view
+from tweets.views import home_view, tweet_action_view, tweet_delete_view, tweet_list_view, tweet_detail_view, tweet_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +24,6 @@ urlpatterns = [
     path('create-tweet', tweet_create_view),
     path('tweets/', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
+    path('api/tweets/action', tweet_action_view),
     path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
 ]
