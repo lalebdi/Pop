bb835c9534763a357a7f1f01db39e1f8c7f62e64
lalebdi <loai.alebdi@gmail.com>
2021-02-17 10:47:39 -0500

    add the trailing slash - oops 😅

--- tweets/urls.py
+++ tweets/urls.py
@@ -20,8 +20,8 @@ from .views import home_view, tweet_action_view, tweet_delete_view, tweet_list_v
 
 urlpatterns = [
     path('', tweet_list_view),
-    path('action', tweet_action_view),
-    path('create', tweet_create_view),
-    path('<int:tweet_id>', tweet_detail_view),
-    path('<int:tweet_id>/delete', tweet_delete_view),
+    path('action/', tweet_action_view),
+    path('create/', tweet_create_view),
+    path('<int:tweet_id>/', tweet_detail_view),
+    path('<int:tweet_id>/delete/', tweet_delete_view),
 ]
