394a53460c40f98adcaed821b900481c778cced0
lalebdi <loai.alebdi@gmail.com>
2021-02-16 20:39:09 -0500

    debug and retweet functionality

--- tweets/views.py
+++ tweets/views.py
@@ -65,9 +65,10 @@ def tweet_action_view(request, *args, **kwargs):
     # print(request.POST, request.data)
     serializer = TweetActionSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
-        date = serializer.validated_data
-        tweet_id = date.get("id")
-        action = date.get("action")
+        data = serializer.validated_data
+        tweet_id = data.get("id")
+        action = data.get("action")
+        content = data.get("content")
         qs = Tweet.objects.filter(id=tweet_id)
         if not qs.exists():
             return Response({}, status=404)
@@ -79,7 +80,10 @@ def tweet_action_view(request, *args, **kwargs):
         elif action == "unlike":
             obj.likes.remove(request.user)
         elif action == "retweet":
-            pass # need to figure this out!!
+            parent_obj = obj
+            new_tweet = Tweet.objects.create(user=request.user, parent=parent_obj, content=content)
+            serializer = TweetSerializer(new_tweet)
+            return Response(serializer.data, status=200)
     return Response({"message": "Action Happened"}, status=200)
 
 
