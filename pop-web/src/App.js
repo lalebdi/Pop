61aeac64da49cb7896d953208897304f6eac3287
lalebdi <loai.alebdi@gmail.com>
2021-02-18 16:07:43 -0500

    add action button

--- pop-web/src/App.js
+++ pop-web/src/App.js
@@ -21,11 +21,21 @@ function loadTweets(callback){
 
   }
 
+  function ActionBtn(props){
+    const {tweet, action} = props
+    const className = props.className ? props.className : 'btn btn-primary btn-sm'
+    return action.type === 'like' ? <button className={className}> { tweet.likes } Likes </button> : null
+}
+
   function Tweet(props){
     const {tweet} = props
     const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+    const action = {type: "like"}
     return <div className={className}>
             <p>{tweet.content}</p>
+            <div className='btn btn-group'>
+              <ActionBtn tweet={tweet} action={action}/>
+            </div>
     </div>
   }
 
@@ -34,7 +44,7 @@ function App() {
 
   useEffect(() => {
     const myCallback = (response, status) =>{
-      console.log(response, status)
+      // console.log(response, status)
       if (status === 200){
         setTweets(response)
       }
