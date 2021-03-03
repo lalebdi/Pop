a286352c21d58ce896a6633bff6d3e28e5ee779a
lalebdi <loai.alebdi@gmail.com>
2021-03-02 20:51:55 -0500

    move the lookup function to a component

--- pop-web/src/tweets/components.js
+++ pop-web/src/tweets/components.js
@@ -1,23 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { loadTweets } from '../lookup'
 
-export function loadTweets(callback){
-        const xhr = new XMLHttpRequest()
-        const method = 'GET'
-        const url = 'http://127.0.0.1:8000/api/tweets/'
-        const responseType = "json"
-    
-        xhr.responseType = responseType
-        xhr.open(method, url)
-        xhr.onload = function(){
-        callback(xhr.response, xhr.status)
-        }
-        xhr.onerror = function(e) {
-        console.log(e)
-        callback({"message": "The request encoutered an error"}, 400)
-        }
-        xhr.send()
-    
-        }
+
     
 export function TweetList(props){
         const [ tweets, setTweets ] = useState([])
