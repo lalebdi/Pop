09eafcbb9c84531e3becf57510bb6252a8a3c1fb
lalebdi <loai.alebdi@gmail.com>
2021-03-02 20:43:03 -0500

    place the new functions here

--- pop-web/src/tweets/components.js
+++ pop-web/src/tweets/components.js
@@ -1,4 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+export function loadTweets(callback){
+        const xhr = new XMLHttpRequest()
+        const method = 'GET'
+        const url = 'http://127.0.0.1:8000/api/tweets/'
+        const responseType = "json"
+    
+        xhr.responseType = responseType
+        xhr.open(method, url)
+        xhr.onload = function(){
+        callback(xhr.response, xhr.status)
+        }
+        xhr.onerror = function(e) {
+        console.log(e)
+        callback({"message": "The request encoutered an error"}, 400)
+        }
+        xhr.send()
+    
+        }
+    
+export function TweetList(props){
+        const [ tweets, setTweets ] = useState([])
+    
+        useEffect(() => {
+        const myCallback = (response, status) =>{
+            // console.log(response, status)
+            if (status === 200){
+            setTweets(response)
+            }
+        }
+        loadTweets(myCallback)
+        }, [])
+    
+        return tweets.map((item, index)=>{
+        return <Tweet tweet={item} key={`${index}-{item.id}`} className='my-5 py-5 border bg-white text-dark'/>
+        })
+        }
 
 
 export function ActionBtn(props){
