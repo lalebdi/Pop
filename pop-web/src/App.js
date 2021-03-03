903321d4d879cb4c4c9275a2fc62e956d375f74a
lalebdi <loai.alebdi@gmail.com>
2021-03-02 20:31:13 -0500

    move tweet and actionBtn to a new component

--- pop-web/src/App.js
+++ pop-web/src/App.js
@@ -20,24 +20,6 @@ function loadTweets(callback){
 
   }
 
-  function ActionBtn(props){
-    const {tweet, action} = props
-    const className = props.className ? props.className : 'btn btn-primary btn-sm'
-    return action.type === 'like' ? <button className={className}> { tweet.likes } Likes </button> : null
-}
-
-  function Tweet(props){
-    const {tweet} = props
-    const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
-    const action = {type: "like"}
-    return <div className={className}>
-            <p>{tweet.content}</p>
-            <div className='btn btn-group'>
-              <ActionBtn tweet={tweet} action={action}/>
-            </div>
-    </div>
-  }
-
 function App() {
   const [ tweets, setTweets ] = useState([])
 
