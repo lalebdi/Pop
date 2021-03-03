15684c50de96ab43ec578e90ef79fc64c5703d58
lalebdi <loai.alebdi@gmail.com>
2021-03-02 20:42:49 -0500

    remove functions and refactor code

--- pop-web/src/App.js
+++ pop-web/src/App.js
@@ -1,43 +1,8 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
-import { Tweet } from './tweets'
+import { TweetList } from './tweets'
 
-function loadTweets(callback){
-  const xhr = new XMLHttpRequest()
-  const method = 'GET'
-  const url = 'http://127.0.0.1:8000/api/tweets/'
-  const responseType = "json"
 
-  xhr.responseType = responseType
-  xhr.open(method, url)
-  xhr.onload = function(){
-  callback(xhr.response, xhr.status)
-  }
-  xhr.onerror = function(e) {
-    console.log(e)
-    callback({"message": "The request encoutered an error"}, 400)
-  }
-  xhr.send()
-
-  }
-
-  function TweetList(props){
-    const [ tweets, setTweets ] = useState([])
-
-  useEffect(() => {
-    const myCallback = (response, status) =>{
-      // console.log(response, status)
-      if (status === 200){
-        setTweets(response)
-      }
-    }
-    loadTweets(myCallback)
-  }, [])
-
-  return tweets.map((item, index)=>{
-    return <Tweet tweet={item} key={`${index}-{item.id}`} className='my-5 py-5 border bg-white text-dark'/>
-  })
-  }
 
 function App() {
   
