837e3c0a99eafb94f84c29a15f642fa1aa2d2f7c
lalebdi <loai.alebdi@gmail.com>
2021-03-20 22:51:46 -0400

    plug react url pattern

--- pop/urls.py
+++ pop/urls.py
@@ -13,14 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from tweets.views import home_view, tweet_action_view, tweet_delete_view, tweet_list_view, tweet_detail_view, tweet_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
+    path('react/', TemplateView.as_view(template_name='react.html')),
     path('create-tweet', tweet_create_view),
     path('tweets/', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
@@ -28,3 +33,7 @@ urlpatterns = [
     # path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
     path('api/tweets/', include('tweets.urls'))
 ]
+
+# Below I am adding the static files
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
