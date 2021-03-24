c863c872d36ee8c202c3612ceeee207b03676c40
lalebdi <loai.alebdi@gmail.com>
2021-03-23 20:57:22 -0400

    create the lookup function

--- pop-web/src/lookup/components.js
+++ pop-web/src/lookup/components.js
@@ -1,13 +1,15 @@
 // import React from 'react';
 
-
-export function loadTweets(callback){
+function lookup(method, endpoint, callback, data){
+    let jsonData;
+    if(data){
+        jsonData = JSON.stringify(data)
+    }
     const xhr = new XMLHttpRequest()
-    const method = 'GET'
-    const url = 'http://127.0.0.1:8000/api/tweets/'
-    const responseType = "json"
+    const url = `http://127.0.0.1:8000/api${endpoint}`
+    // const responseType = "json"
 
-    xhr.responseType = responseType
+    xhr.responseType = "json"
     xhr.open(method, url)
     xhr.onload = function(){
     callback(xhr.response, xhr.status)
@@ -16,6 +18,27 @@ export function loadTweets(callback){
     console.log(e)
     callback({"message": "The request encoutered an error"}, 400)
     }
-    xhr.send()
+    xhr.send(jsonData)
+}
+
+
+export function loadTweets(callback){
+
+    lookup("GET", "/tweets/", callback)
+    // const xhr = new XMLHttpRequest()
+    // const method = 'GET'
+    // const url = 'http://127.0.0.1:8000/api/tweets/'
+    // const responseType = "json"
+
+    // xhr.responseType = responseType
+    // xhr.open(method, url)
+    // xhr.onload = function(){
+    // callback(xhr.response, xhr.status)
+    // }
+    // xhr.onerror = function(e) {
+    // console.log(e)
+    // callback({"message": "The request encoutered an error"}, 400)
+    // }
+    // xhr.send()
 
 }
