8c3252dae798da0a396cc0e18827c1a3928cfc7f
lalebdi <loai.alebdi@gmail.com>
2021-03-28 22:18:40 -0400

    looking for error source

--- pop-web/src/lookup/components.js
+++ pop-web/src/lookup/components.js
@@ -42,6 +42,7 @@ function lookup(method, endpoint, callback, data){
     console.log(e)
     callback({"message": "The request encoutered an error"}, 400)
     }
+    console.log(jsonData)
     xhr.send(jsonData)
 }
 
