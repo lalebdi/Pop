6e885d660f31f6d9aa8d05466632991be52d49e9
lalebdi <loai.alebdi@gmail.com>
2021-03-28 22:25:14 -0400

    refactor fucntions

--- pop-web/src/lookup/components.js
+++ pop-web/src/lookup/components.js
@@ -17,7 +17,7 @@ function getCookie(name) { // <- Copied from Django documention
 }
 
 
-function lookup(method, endpoint, callback, data){
+export function backendLookup(method, endpoint, callback, data){
     let jsonData;
     if(data){
         jsonData = JSON.stringify(data)
@@ -47,27 +47,3 @@ function lookup(method, endpoint, callback, data){
 }
 
 
-export function createTweet(newTweet, callback){
-    lookup("POST", "/tweets/create/", callback, {content: newTweet})
-}
-
-export function loadTweets(callback){
-
-    lookup("GET", "/tweets/", callback)
-    // const xhr = new XMLHttpRequest()
-    // const method = 'GET'
-    // const url = 'http://127.0.0.1:8000/api/tweets/'
-    // const responseType = "json"
-
-    // xhr.responseType = responseType
-    // xhr.open(method, url)
-    // xhr.onload = function(){
-    // callback(xhr.response, xhr.status)
-    // }
-    // xhr.onerror = function(e) {
-    // console.log(e)
-    // callback({"message": "The request encoutered an error"}, 400)
-    // }
-    // xhr.send()
-
-}
