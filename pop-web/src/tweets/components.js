c9723dcdfc7d2431b1575c4b386c4ba81788b0ec
lalebdi <loai.alebdi@gmail.com>
2021-04-14 21:03:47 -0400

    API action Btn

--- pop-web/src/tweets/components.js
+++ pop-web/src/tweets/components.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { apiTweetCreate, apiTweetList } from './lookup'
+import { apiTweetCreate, apiTweetAction, apiTweetList } from './lookup'
 
 export function TweetsComponent(props){
     const textAreaRef = React.createRef()
@@ -81,21 +81,32 @@ export function TweetList(props){
 export function ActionBtn(props){
     const {tweet, action} = props;
     const [likes, setLikes] = useState( tweet.likes ? tweet.likes : 0);
-    const [userLiked, setUserLiked] = useState(tweet.userLiked === true ? true : false);
+    // const [userLiked, setUserLiked] = useState(tweet.userLiked === true ? true : false);
     const className = props.className ? props.className : 'btn btn-primary btn-sm'
     const actionDisplay = action.display ? action.display : 'Action'
     // const display = action.type === 'like' ? `${ tweet.likes } ${actionDisplay}` : actionDisplay
+    const handleActionBackendEvent = (response, status) =>{
+        // console.log(response)
+        // console.log(status)
+        if (status === 200) {
+            setLikes(response.likes)
+            // setUserLiked(true)
+        }
+        // if (action.type === 'like'){ //console.log(tweet.likes+1)
+        //     if (userLiked === true){ // the user unliked 
+        //         setLikes(likes - 1)
+        //         setUserLiked(false)
+        //     } else {
+        //         setLikes(likes + 1)
+        //         setUserLiked(true)    
+        //     }
+        // }
+
+    }
     const handleClick = (event) => {
         event.preventDefault()
-        if (action.type === 'like'){ //console.log(tweet.likes+1)
-            if (userLiked === true){ // the user unliked 
-                setLikes(likes - 1)
-                setUserLiked(false)
-            } else {
-                setLikes(likes + 1)
-                setUserLiked(true)    
-            }
-        }
+        apiTweetAction(tweet.id, action.type, handleActionBackendEvent);
+        
     }
     const display = action.type === 'like' ? `${ likes } ${actionDisplay}` : actionDisplay
     return <button className={className} onClick={handleClick}> {display} </button>
