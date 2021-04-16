08388ed0f9be6b34e466fac1a509d0a7464fe0d4
lalebdi <loai.alebdi@gmail.com>
2021-04-16 14:30:00 -0400

    improve the repost functionality

--- pop-web/src/tweets/components.js
+++ pop-web/src/tweets/components.js
@@ -71,9 +71,19 @@ export function TweetList(props){
         apiTweetList(handleTweetListLookup)
     }
         }, [tweetsInit, tweetsDidSet , setTweetsDidSet])
+
+        const handleDidRetweet = (newTweet) => {
+            const updateTweetsInit = [...tweetsInit]
+            updateTweetsInit.unshift(newTweet);
+            setTweetsInit(updateTweetsInit)
+
+            const updateFinalTweets = [...tweets]
+            updateFinalTweets.unshift(tweets);
+            setTweets(updateFinalTweets)
+        }
     
         return tweets.map((item, index)=>{
-        return <Tweet tweet={item} key={`${index}-{item.id}`} className='my-5 py-5 border bg-white text-dark'/>
+        return <Tweet tweet={item} key={`${index}-{item.id}`} didRetweet={handleDidRetweet} className='my-5 py-5 border bg-white text-dark'/>
         })
         }
 
@@ -119,13 +129,13 @@ export function ParentTweet(props){
     return tweet.parent ? <div className="row">
             <div className="col-11 mx-auto p-3 border rounded">
             <p className="mb-0 text-muted small">Repost</p> 
-            <Tweet className={" "} tweet={tweet.parent}/>
+            <Tweet hideActions className={" "} tweet={tweet.parent}/>
             </div>
         </div> : null
 }
 
 export function Tweet(props){
-    const {tweet} = props
+    const { tweet, didRetweet, hideActions } = props
     const [ actionTweet, setActionTweet ] = useState(props.tweet ? props.tweet : null);
     const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
 
@@ -133,7 +143,9 @@ export function Tweet(props){
         if (status === 200) {
             setActionTweet(newActionTweet)
         } else if(status === 201){
-            // Need to let the tweet list know
+            if(didRetweet){
+                didRetweet(newActionTweet)
+            }
         }
     }
 
@@ -142,7 +154,7 @@ export function Tweet(props){
                 <p>{tweet.id} - {tweet.content}</p>
                     <ParentTweet tweet={tweet} />
             </div>
-            {actionTweet && 
+            {(actionTweet && hideActions !== true) && 
                 <div className='btn btn-group'>
                 <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{type: "like", display:"Likes"}}/>
                 <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{type: "unlike", display:"Unlike" }}/>
