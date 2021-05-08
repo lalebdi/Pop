ddff7393d3ac6e91252561d195adef2311697487
lalebdi <loai.alebdi@gmail.com>
2021-05-08 14:03:41 -0400

    init

---
+++ pop-web/src/tweets/list.js
@@ -0,0 +1,43 @@
+
+
+export function TweetList(props){
+    const [ tweetsInit, setTweetsInit ] = useState([]);
+    const [ tweets, setTweets ] = useState([]);
+    const [ tweetsDidSet, setTweetsDidSet ] = useState(false);
+    // console.log(props.newTweets)
+    useEffect(() =>{
+        const final = [...props.newTweets].concat(tweetsInit)
+        if(final.length !== tweets.length){
+        setTweets(final)
+        }
+    }, [props.newTweets , tweets, tweetsInit])
+
+    useEffect(() => {
+        if(tweetsDidSet === false){
+    const handleTweetListLookup = (response, status) =>{
+        // console.log(response, status)
+        if (status === 200){
+        setTweetsInit(response)
+        setTweetsDidSet(true);
+        } else {
+            alert("There was an error 🤦🏼‍♀️")
+        }
+    }
+    apiTweetList(props.username, handleTweetListLookup) // props.username must be passed as a dependecy below to fix the useEffect hook warning
+}
+    }, [tweetsInit, tweetsDidSet , setTweetsDidSet, props.username])
+
+    const handleDidRetweet = (newTweet) => {
+        const updateTweetsInit = [...tweetsInit]
+        updateTweetsInit.unshift(newTweet);
+        setTweetsInit(updateTweetsInit)
+
+        const updateFinalTweets = [...tweets]
+        updateFinalTweets.unshift(tweets);
+        setTweets(updateFinalTweets)
+    }
+
+    return tweets.map((item, index)=>{
+    return <Tweet tweet={item} key={`${index}-{item.id}`} didRetweet={handleDidRetweet} className='my-5 py-5 border bg-white text-dark'/>
+    })
+    };
