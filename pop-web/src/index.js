890c31f90bf112bb2d7de812d915708a3a0d0d0b
lalebdi <loai.alebdi@gmail.com>
2021-05-08 21:36:57 -0400

    Lookup & Render & Embed Individual Tweets

--- pop-web/src/index.js
+++ pop-web/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { TweetsComponent } from './tweets';  
+import { TweetsComponent, TweetDetailComponent } from './tweets';  
 import reportWebVitals from './reportWebVitals';
 
 
@@ -25,6 +25,14 @@ if(tweetsEl){
     // </React.StrictMode>
   );
 }
+
+const tweetDetailElements = document.querySelectorAll(".pop-detail")
+
+tweetDetailElements.forEach(container => {
+  ReactDOM.render(
+    e(TweetDetailComponent, container.dataset), container
+  )
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
