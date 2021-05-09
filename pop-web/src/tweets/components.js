82093043b6f0e927e8c1861d11a5f5d84f83095b
lalebdi <loai.alebdi@gmail.com>
2021-05-08 21:37:00 -0400

    Lookup & Render & Embed Individual Tweets

--- pop-web/src/tweets/components.js
+++ pop-web/src/tweets/components.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TweetList } from './list'
 import { TweetCreate } from './create'
+import { apiTweetDetail } from './lookup'
+import { Tweet } from './detail'
 
 
 export function TweetsComponent(props){
@@ -23,5 +25,28 @@ export function TweetsComponent(props){
     </div>
 }
     
+
+export function TweetDetailComponent (props) {
+    const { tweetId } = props
+    const [didLookup, setDidLookup] = useState(false);
+    const [tweet, setTweet] = useState(null)
+
+    const handleBackendLookup = (response, status) =>{
+        if(status === 200){
+            setTweet(response)
+        } else {
+            alert("There was an error finding your tweet 😢 ")
+        }
+    }
+    useEffect(()=>{
+        if(didLookup === false){
+            apiTweetDetail(tweetId, handleBackendLookup)
+            setDidLookup(true)
+        }
+
+    }, [tweetId, didLookup, setDidLookup]) 
+ 
+    return tweet === null ? null : <Tweet tweet={tweet} className={props.className} />
+}
 
 
