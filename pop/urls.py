90ee4a06a083fbd822c1a22e55bed42bff047afa
lalebdi <loai.alebdi@gmail.com>
2021-05-23 15:25:25 -0400

    generate new views

--- pop/urls.py
+++ pop/urls.py
@@ -20,18 +20,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-from tweets.views import home_view, tweet_action_view, tweet_delete_view, tweet_list_view, tweet_detail_view, tweet_create_view
+from tweets.views import local_tweets_list_view, local_tweets_profile_view, local_tweets_detail_view
+# from tweets.views import home_view, tweet_action_view, tweet_delete_view, tweet_list_view, tweet_detail_view, tweet_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view),
-    path('react/', TemplateView.as_view(template_name='react_via_dj.html')),
-    path('create-tweet', tweet_create_view),
-    path('tweets/', tweet_list_view),
-    path('tweets/<int:tweet_id>', tweet_detail_view),
+    path('', local_tweets_list_view),
+    path('<int:tweet_id>', local_tweets_detail_view),
+    path('profile/<str:username>', local_tweets_profile_view),
+    path('api/tweets/', include('tweets.urls'))
+    # path('', home_view),
+    # path('react/', TemplateView.as_view(template_name='react_via_dj.html')),
+    # path('create-tweet', tweet_create_view),
+    # path('tweets/', tweet_list_view),
+    # path('tweets/<int:tweet_id>', tweet_detail_view),
     # path('api/tweets/action', tweet_action_view),
     # path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
-    path('api/tweets/', include('tweets.urls'))
 ]
 
 # Below I am adding the static files
