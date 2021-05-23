1024481ae9e85d78fac51a3c973ac7cad6d22584
lalebdi <loai.alebdi@gmail.com>
2021-05-23 15:32:44 -0400

    init

---
+++ tweets/api/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import home_view, tweet_action_view, tweet_delete_view, tweet_list_view, tweet_detail_view, tweet_create_view
+
+urlpatterns = [
+    path('', tweet_list_view),
+    path('action/', tweet_action_view),
+    path('create/', tweet_create_view),
+    path('<int:tweet_id>/', tweet_detail_view),
+    path('<int:tweet_id>/delete/', tweet_delete_view),
+]
