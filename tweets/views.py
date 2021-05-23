c5aa78737f5535cdd729c9a8a0ead388d7d9eb49
lalebdi <loai.alebdi@gmail.com>
2021-05-23 15:25:17 -0400

    generate new views

--- tweets/views.py
+++ tweets/views.py
@@ -20,7 +20,22 @@ from .serializers import TweetSerializer, TweetActionSerializer, TweetCreateSeri
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 def home_view(request, *args, **kwargs):
-    return render(request, "pages/home.html", context={}, status=200)
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+    return render(request, "pages/home.html", context={"username": username}, status=200)
+
+# The new local views need to be brought to the configuration
+def local_tweets_list_view(request, *args, **kwargs):
+    return render(request, "tweets/list.html")
+
+
+def local_tweets_profile_view(request, username, *args, **kwargs):
+    return render(request, "tweets/profile.html", context={"profile_username" : username})
+
+
+def local_tweets_detail_view(request, tweet_id, *args, **kwargs):
+    return render(request, "tweets/detail.html", context={"tweet_id" : tweet_id})
 
 
 @api_view(['POST']) # That means the method the client sends == POST
