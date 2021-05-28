12e1f8ed682f8dbd2bf192430ef078e4aea86ff5
lalebdi <loai.alebdi@gmail.com>
2021-05-28 15:44:10 -0400

    Authentication & Registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
-# Create your views here.
+# Create your views here. Consider using authenticate with the login/ logout
+
+
+def login_view(request, *args, **kwargs):
+    form = AuthenticationForm(request, data=request.POST or None)   # This is standard
+    if form.is_valid():
+        user_ = form.get_user()
+        login(request, user_)
+        return redirect("/")
+    context = {
+        "form": form,
+        "btn_label": "Login",
+        "title": "Login"
+        }
+    return render(request, "accounts/auth.html", context)
+
+
+def logout_view(request, *args, **kwargs):
+    if request.method == "POST":
+        logout(request)
+        return redirect("/login")
+    context = {
+        "form": None,
+        "description": "Are you sure you want to logout?",
+        "btn_label": "Click logout to confirm",
+        "title": "Logout"
+        }
+    return render(request, "accounts/auth.html", context)
+
+
+def register_view(request, *args, **kwargs):
+    form = UserCreationForm(request.POST or None)   
+    if form.is_valid():
+        print(form.cleaned_data)
+    context = {
+        "form": form,
+        "btn_label": "Register",
+        "title": "Register"
+        }
+    return render(request, "accounts/auth.html", context)
