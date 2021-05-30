9f718172d45867431551c489fe312f06052cd58b
lalebdi <loai.alebdi@gmail.com>
2021-05-30 17:16:13 -0400

    add signals to the profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render
-
-from .models import Profile
+from django.db.models.signals import post_save
+from .models import Profile, User
 # Create your views here.
 
 
@@ -15,3 +15,12 @@ def profile_detail_view(request, username, *args, **kwargs):
         "profile": profile_obj
         }
     return render(request, "profiles/detail.html", context)
+
+
+def user_did_save(sender, instance, created,*args, **kwargs):
+    # Profile.objects.get_or_create(user= instance)
+    if created:
+        Profile.objects.get_or_create(user= instance)
+
+
+post_save.connect(user_did_save, sender= User)
