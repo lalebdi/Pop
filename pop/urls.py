1d239c26ca075ca50e63f25d44159e2968616cf9
lalebdi <loai.alebdi@gmail.com>
2021-05-30 16:53:48 -0400

    use regex for the profiles

--- pop/urls.py
+++ pop/urls.py
@@ -18,9 +18,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.urls.conf import re_path
 from django.views.generic import TemplateView
 
-from tweets.views import tweets_list_view, tweets_profile_view, tweets_detail_view
+from tweets.views import tweets_list_view,  tweets_detail_view, #tweets_profile_view
 from accounts.views import login_view, logout_view, register_view
 # from tweets.views import home_view, tweet_action_view, tweet_delete_view, tweet_list_view, tweet_detail_view, tweet_create_view
 
@@ -31,7 +32,7 @@ urlpatterns = [
     path('logout/', logout_view),
     path('register/', register_view),
     path('<int:tweet_id>', tweets_detail_view),
-    path('profile/<str:username>', tweets_profile_view),
+    re_path(r'profiles?/', include('profiles.urls')),
     path('api/tweets/', include('tweets.api.urls'))
     # path('', home_view),
     # path('react/', TemplateView.as_view(template_name='react_via_dj.html')),
