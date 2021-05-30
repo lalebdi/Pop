d024db108ee2aff6813d328bb97bf6f96e6c48c7
lalebdi <loai.alebdi@gmail.com>
2021-05-30 16:39:35 -0400

    profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,12 @@
+
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.conf import settings
 
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # One to One is for one user to have one profile
+    location = models.CharField(max_length=220, null=True, blank=True)
+    bio = models.CharField(max_length=220, null=True, blank=True)
