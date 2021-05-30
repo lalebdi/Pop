67629573361607f4658a74847898858042eb28ae
lalebdi <loai.alebdi@gmail.com>
2021-05-30 17:00:17 -0400

    handle user not found

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,17 @@
+from django.http import Http404
 from django.shortcuts import render
 
+from .models import Profile
 # Create your views here.
 
 
 def profile_detail_view(request, username, *args, **kwargs):
-# get the profile to the username
-    return render(request, "profiles/detail.html", {"username": username})
+    qs = Profile.objects.filter(user__username = username) # user__username for the foreign key relationship
+    if not qs.exists():
+        raise Http404
+    profile_obj = qs.first()
+    context = {
+        "username": username,
+        "profile": profile_obj
+        }
+    return render(request, "profiles/detail.html", context)
