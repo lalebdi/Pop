cee9d24110520ac106a5149a14b61d002eb78d0c
lalebdi <loai.alebdi@gmail.com>
2021-05-30 16:45:13 -0400

    init

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def profile_detail_view(request, username, *args, **kwargs):
+# get the profile to the username
+    return render(request, "profiles/detail.html", {"username": username})
