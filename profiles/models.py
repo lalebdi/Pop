872197308e77f60a0e5b332acd2bb3058346ed4a
lalebdi <loai.alebdi@gmail.com>
2021-05-31 15:40:19 -0400

    add the following

--- profiles/models.py
+++ profiles/models.py
@@ -6,7 +6,16 @@ from django.conf import settings
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
+
+class FollowerRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) # One to One is for one user to have one profile
     location = models.CharField(max_length=220, null=True, blank=True)
-    bio = models.CharField(max_length=220, null=True, blank=True)
+    bio = models.TextField(max_length=220, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    followers = models.ManyToManyField(User, related_name='following', blank=True)
