da8473a93cb4b1ee6302fce67e309a4be808bfee
lalebdi <loai.alebdi@gmail.com>
2021-05-31 15:03:27 -0400

    pip guidlines

--- tweets/tests.py
+++ tweets/tests.py
@@ -17,10 +17,12 @@ class TweetTestCase(TestCase):
         Tweet.objects.create(content="test fourth tweet", user=self.userB) # adding tweet objects to test the tweet list -> id =3
         self.currentCount = Tweet.objects.all().count()
 
+
     def test_user_created(self):
         # user = User.objects.get(username='efg')
         self.assertEqual(self.user.username, "efg") # assert Equal comes inhereted from TestCase
         # self.assertEqual(1, 2)
+
 
     def test_tweet_created(self):
         tweet_obj = Tweet.objects.create(content="test second tweet", user=self.user)
@@ -28,14 +30,17 @@ class TweetTestCase(TestCase):
         self.assertEqual(tweet_obj.id, 4) # adding tweet objects to test the tweet list
         self.assertEqual(tweet_obj.user, self.user)
 
+
     def test_api_login(self):
         client = APIClient()
         client.login(username=self.user.username, password='somepassword')
+
 
     def get_client(self):
         client = APIClient()
         client.login(username=self.user.username, password='somepassword')
         return client
+
 
     def test_tweet_list(self):
         client = self.get_client()
@@ -52,6 +57,7 @@ class TweetTestCase(TestCase):
         self.assertEqual(like_count, 1)
         # print(response.json())
 
+
     def test_action_unlike(self):
         client = self.get_client()
         response = client.post("/api/tweets/action/", {"id": 2, "action": "like"}) # I have to first hit like then unlike it
@@ -61,6 +67,7 @@ class TweetTestCase(TestCase):
         like_count = response.json().get("likes")
         self.assertEqual(like_count, 0)
         # print(response.json())
+
 
     def test_action_retweet(self):
         client = self.get_client()
@@ -72,6 +79,7 @@ class TweetTestCase(TestCase):
         self.assertNotEqual(2, new_tweet_id)
         self.assertEqual(currentCount + 1, new_tweet_id) # after creating the objects above, retweeting should add the number of objects
 
+
     def test_tweet_create_api_view(self):
         request_data = {"content": "THis is a test tweet"}
         client = self.get_client()
@@ -81,6 +89,7 @@ class TweetTestCase(TestCase):
         new_tweet_id = response_data.get("id")
         self.assertEqual(self.currentCount + 1, new_tweet_id)
 
+
     def test_tweet_detail_api_view(self):
         client = self.get_client()
         response = client.get("/api/tweets/1/")
@@ -88,6 +97,7 @@ class TweetTestCase(TestCase):
         data = response.json()
         _id = data.get("id")
         self.assertEqual(_id, 1)
+
 
     def test_tweet_delete(self):
         client = self.get_client()
@@ -99,6 +109,7 @@ class TweetTestCase(TestCase):
         client = self.get_client()
         response = client.delete("/api/tweets/1/delete/")
         self.assertEqual(response.status_code, 404)
+
 
         response_incorrect_user = client.delete("/api/tweets/3/delete/")
         self.assertEqual(response_incorrect_user.status_code, 401) # Permission error
