c1f7869635f8ccc3a3738a48fabc85fa56edb0df
lalebdi <loai.alebdi@gmail.com>
2021-05-31 14:45:53 -0400

    2 Models in 1 Form and 1 View

--- profiles/forms.py
+++ profiles/forms.py
@@ -6,10 +6,26 @@ from .models import Profile
 
 User = get_user_model()
 
+class UserProfileForm(forms.ModelForm):
+    locaton = forms.CharField(required=False)
+    bio = forms.CharField(required=False)
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+
 class ProfileForm(forms.ModelForm):
     first_name = forms.CharField(required=False)
     last_name = forms.CharField(required=False)
-    email_address = forms.CharField(required=False)
+    email = forms.CharField(required=False)
     class Meta:
         model = Profile
         fields = ['location', 'bio']
+
+
+class ProfileBasicForm(forms.Form):
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+    email_address = forms.CharField(required=False)
+    locaton = forms.CharField(required=False)
+    bio = forms.CharField(required=False)
