7ebe1399cd730b05fc433e257a429621e3580ec9
lalebdi <loai.alebdi@gmail.com>
2021-05-31 14:45:50 -0400

    2 Models in 1 Form and 1 View

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path, include
-from .views import profile_detail_view
+from .views import profile_detail_view, profile_update_view
+
 
 
 urlpatterns = [
+    path('edit', profile_update_view),
     path('<str:username>', profile_detail_view),
     # path('', tweets_list_view),
 ]
