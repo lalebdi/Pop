652c98d8d9e0df2287b64e096c84af4494d564b3
lalebdi <loai.alebdi@gmail.com>
2021-06-01 22:17:48 -0400

    Follow Button Logic and Endpoint

--- profiles/api/urls.py
+++ profiles/api/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import tweet_action_view, tweet_delete_view, tweet_list_view, tweet_detail_view, tweet_create_view
+from .views import user_follow_view
 
 urlpatterns = [
-    path('', tweet_list_view),
-    path('action/', tweet_action_view),
-    path('create/', tweet_create_view),
-    path('<int:tweet_id>/', tweet_detail_view),
-    path('<int:tweet_id>/delete/', tweet_delete_view),
+    path('<str:username>/follow', user_follow_view),
 ]
