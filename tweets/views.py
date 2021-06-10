ce0e25caeb55993d7153551fde5f76c72b89c0e9
lalebdi <loai.alebdi@gmail.com>
2021-06-09 21:09:16 -0400

    move the authentication to django and the template

--- tweets/views.py
+++ tweets/views.py
@@ -20,10 +20,11 @@ from django.shortcuts import render, redirect
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 def home_view(request, *args, **kwargs):
-    username = None
-    if request.user.is_authenticated:
-        username = request.user.username
-    return render(request, "pages/home.html", context={"username": username}, status=200)
+    # username = None
+    # if request.user.is_authenticated:
+    #     username = request.user.username
+    # return render(request, "pages/feed.html", context={"username": username}, status=200)
+    return render(request, "pages/feed.html")
 
 # The new local views need to be brought to the configuration
 def tweets_list_view(request, *args, **kwargs):
