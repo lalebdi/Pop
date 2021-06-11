ff765281c1ecb95a3a63b6b50b587171e100f022
lalebdi <loai.alebdi@gmail.com>
2021-06-10 22:22:49 -0400

    create the user profile details

--- profiles/api/urls.py
+++ profiles/api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import user_follow_view
+from .views import user_follow_view, profile_detail_api_view
 
 urlpatterns = [
     path('<str:username>/follow', user_follow_view),
+    path('<str:username>/', profile_detail_api_view), # TODO:need to fix the bug here. it is case sensitive. 
 ]
