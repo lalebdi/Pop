4a31048fa35ee1cd0525cf583c1beea3315bdd21
lalebdi <loai.alebdi@gmail.com>
2021-06-14 20:48:11 -0400

    create the profile badge

--- pop-web/src/profiles/badge.js
+++ pop-web/src/profiles/badge.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { apiProfileDetail } from './lookup'
 
 export default function ProfileBadge(props) {
     const { username } = props
-    return (
-        <div>
-            
-        </div>
-    )
+
+    // doing the lookup
+    const [didLookup, setDidLookup] = useState(false);
+    const [profile, setProfile] = useState(null)
+
+    const handleBackendLookup = (response, status) =>{
+        if(status === 200){
+            setProfile(response)
+        } 
+    }
+    useEffect(()=>{
+        if(didLookup === false){
+            apiProfileDetail(username, handleBackendLookup)
+            setDidLookup(true)
+        }
+
+    }, [username, didLookup, setDidLookup]) 
+    return profile ? <span>{profile.username}</span> : ""
+    
 }
