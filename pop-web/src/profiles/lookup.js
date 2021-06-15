9424505438f72ced9f2e554f7c71174c822adbd8
lalebdi <loai.alebdi@gmail.com>
2021-06-15 15:21:52 -0400

    follower button

--- pop-web/src/profiles/lookup.js
+++ pop-web/src/profiles/lookup.js
@@ -1,6 +1,15 @@
-import { backendLookup } from '../lookup';
+import {backendLookup} from '../lookup'
 
-
-export function apiProfileDetail(username ,callback){
+export function apiProfileDetail(username, callback) {
     backendLookup("GET", `/profiles/${username}/`, callback)
 }
+
+
+export function apiProfileFollowToggle(username, action, callback) {
+    const data = {action: `${action && action}`.toLowerCase()}
+    backendLookup("POST", `/profiles/${username}/follow`, callback, data)
+}
+
+
+
+// template literal to avoid errors when passing null here -> it will be an empty string.
