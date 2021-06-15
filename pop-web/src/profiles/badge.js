7a94f0fdc6aec2cb7a5f40d1042e082c5ed7e399
lalebdi <loai.alebdi@gmail.com>
2021-06-15 15:22:12 -0400

    follower button

--- pop-web/src/profiles/badge.js
+++ pop-web/src/profiles/badge.js
@@ -1,52 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import { apiProfileDetail } from './lookup'
-import { UserDisplay, UserPicture } from './components'
+import React, {useEffect, useState} from 'react'
 
+import {UserDisplay, UserPicture} from './components'
+import {apiProfileDetail, apiProfileFollowToggle} from './lookup'
 
-export function ProfileBadge(props){
-    const { user, didFollowToggle, profileLoading } = props
-    // console.log(user)
-    const currentVerb = (user && user.is_following) ? "Unfollow" : "Follow"
-    const handleFollowToggle = (event) =>{
-        // console.log(event)
+function ProfileBadge(props) {
+    const {user, didFollowToggle, profileLoading} = props
+    let currentVerb = (user && user.is_following) ? "Unfollow" : "Follow"
+    currentVerb = profileLoading ? "Loading..." : currentVerb
+    const handleFollowToggle = (event) => {
         event.preventDefault()
         if (didFollowToggle && !profileLoading) {
             didFollowToggle(currentVerb)
         }
     }
     return user ? <div>
-            <UserPicture user={user} hideLink />
-            <p><UserDisplay user={user} inludeFullName hideLink/></p>
-            <button className='btn btn-primary' onClick={handleFollowToggle}>{currentVerb}</button>
-        </div> : null
+        <UserPicture user={user} hideLink />
+        <p><UserDisplay user={user} includeFullName hideLink /></p>
+        <p>{user.location}</p>
+        <p>{user.bio}</p>
+        <button className='btn btn-primary' onClick={handleFollowToggle}>{currentVerb}</button>
+    </div> : null
 }
 
-export function ProfileBadgeComponent(props) { // !parent component -> should handle the lookup
-    const { username } = props
-
-    // doing the lookup
-    const [didLookup, setDidLookup] = useState(false);
+export function ProfileBadgeComponent (props) {
+    const {username} = props
+    // lookup
+    const [didLookup, setDidLookup] = useState(false)
     const [profile, setProfile] = useState(null)
     const [profileLoading, setProfileLoading] = useState(false)
-
-    const handleBackendLookup = (response, status) =>{
-        if(status === 200){
-            setProfile(response)
-        } 
+    const handleBackendLookup = (response, status) => {
+        if (status === 200) {
+        setProfile(response)
+        }
     }
     useEffect(()=>{
-        if(didLookup === false){
-            apiProfileDetail(username, handleBackendLookup)
-            setDidLookup(true)
+        if (didLookup === false){
+        apiProfileDetail(username, handleBackendLookup)
+        setDidLookup(true)
         }
-
-    }, [username, didLookup, setDidLookup]) 
+    }, [username, didLookup, setDidLookup])
 
     const handleNewFollow = (actionVerb) => {
-        console.log(actionVerb)
+        apiProfileFollowToggle(username, actionVerb, (response, status)=>{
+            // console.log(response, status)
+            if (status===200) {
+                setProfile(response)
+                // apiProfileDetail(username, handleBackendLookup)
+            }
+            setProfileLoading(false)
+        })
         setProfileLoading(true)
+        
     }
-
-    return didLookup === false ? "Loading... 🤓" : profile ? <ProfileBadge user={profile} didFollowToggle={handleNewFollow} profileLoading={profileLoading} /> : null
-    
+    return didLookup === false ? "Loading..." : profile ? <ProfileBadge user={profile} didFollowToggle={handleNewFollow} profileLoading={profileLoading} /> : null
 }
