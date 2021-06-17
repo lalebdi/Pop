9c6e2d34a22b97bf81c180656cbdbd44b0f4bf7b
lalebdi <loai.alebdi@gmail.com>
2021-06-16 20:55:51 -0400

    consolidate the url to one view method

--- profiles/api/urls.py
+++ profiles/api/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import user_follow_view, profile_detail_api_view
+from .views import profile_detail_api_view #user_follow_view
 
 urlpatterns = [
-    path('<str:username>/follow', user_follow_view),
+    path('<str:username>/follow', profile_detail_api_view),
     path('<str:username>/', profile_detail_api_view), # TODO:need to fix the bug here. it is case sensitive. 
 ]
