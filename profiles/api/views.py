950ef7a0774339800f1ca112c1755fb753da34f2
lalebdi <loai.alebdi@gmail.com>
2021-06-16 20:54:13 -0400

    Remove Redundant Profile View

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -36,40 +36,36 @@ def profile_detail_api_view(request, username, *args, **kwargs):
     return Response(serializer.data, status=200)
 
 
-@api_view(['GET','POST']) # That means the method the client sends == POST
-# @authentication_classes([SessionAuthentication])
-@permission_classes([IsAuthenticated]) # if authenticated, they have access to this
-def user_follow_view(request, username, *args, **kwargs): # <- REST Framework handling this
-    me = request.user
-    other_user_qs = User.objects.filter(username=username)
-    if me.username == username: # to prevent following yourself
-        my_followers = me.profile.followers.all()
-        return Response({"count": my_followers.count()}, status=200)
+# @api_view(['GET','POST']) # That means the method the client sends == POST
+# # @authentication_classes([SessionAuthentication])
+# @permission_classes([IsAuthenticated]) # if authenticated, they have access to this
+# def user_follow_view(request, username, *args, **kwargs): # <- REST Framework handling this
+#     me = request.user
+#     other_user_qs = User.objects.filter(username=username)
+#     if me.username == username: # to prevent following yourself
+#         my_followers = me.profile.followers.all()
+#         return Response({"count": my_followers.count()}, status=200)
 
-    if not other_user_qs.exists():
-        return Response({}, status=404)
-    other = other_user_qs.first()
-    profile = other.profile
-    data = {}
-    try: # can use (data = request.data or {}) instead
-        data = request.data
-    except:
-        pass
-    # print(data)
-    action = data.get("action")
-    if action == "follow":
-        profile.followers.add(me)
-    elif action == "unfollow":
-        profile.followers.remove(me)
-    else:
-        pass
-    # current_followers_qs = profile.followers.all()
-    data = PublicProfileSerializer(instance=profile, context={"request": request})
-    return Response(data.data, status=200)
-
-
-
-
+#     if not other_user_qs.exists():
+#         return Response({}, status=404)
+#     other = other_user_qs.first()
+#     profile = other.profile
+#     data = {}
+#     try: # can use (data = request.data or {}) instead
+#         data = request.data
+#     except:
+#         pass
+#     # print(data)
+#     action = data.get("action")
+#     if action == "follow":
+#         profile.followers.add(me)
+#     elif action == "unfollow":
+#         profile.followers.remove(me)
+#     else:
+#         pass
+#     # current_followers_qs = profile.followers.all()
+#     data = PublicProfileSerializer(instance=profile, context={"request": request})
+#     return Response(data.data, status=200)
 
 
 
